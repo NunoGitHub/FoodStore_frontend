1a49bbf2786ead7ac55507deda28348017194ab6
NunoGitHub <nunoaapp22@hotmail.com>
2024-07-25 16:52:11 +0100

    add cart page and title

--- src/app/components/pages/cart-page/cart-page.component.ts
+++ src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
+import { CartItem } from './../../../shared/models/CartItem';
+import { CartService } from './../../../services/cart.service';
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/shared/Cart';
 
 @Component({
   selector: 'app-cart-page',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPageComponent implements OnInit {
 
-  constructor() { }
+  cart!: Cart;
+  quantities: number[] = [1, 2, 3, 4, 5];
+
+  constructor(private cartService:CartService) {
+    //update the cart each time we got a new value, and the observable do that
+    cartService.getCartObservable().subscribe((cart) =>{
+      this.cart =cart;
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  removeFromCart(cartItem:CartItem){
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartItem:CartItem, quantityInString:string){
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+  }
 }
